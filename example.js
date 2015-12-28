1990ef590da67c5348f47067c73a8ebb43d922a8
弱冠 <qianshan.wengqs@alibaba-inc.com>
2015-12-28 15:05:05 +0800

    example up

--- example.js
+++ example.js
@@ -1,9 +1,22 @@
 var App = require('./index.js');
 var app = new App('App Key', 'App Secret');
 
+//发短信
 app.smsSend({
     sms_free_sign_name: '注册验证',
     sms_param: JSON.stringify({"code": "123456", "product": "测试网站"}),
-    rec_num: '13599999999',
+    rec_num: '13888888888',
     sms_template_code: 'SMS_640004'
+}, function(res) {
+    console.log(res)
 });
+
+//查询发送记录
+app.smsQuery({
+    rec_num: '13888888888',
+    query_date: '20151228',
+    current_page: 1,
+    page_size: 20
+}, function(res) {
+    console.log(res['alibaba_aliqin_fc_sms_num_query_response'].values)
+})
